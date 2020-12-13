00527cce243c5ac4d3d67314c2822ae50452f1c5
Elijah Harrison <ehrrsn7@gmail.com>
2020-12-12 17:45:59 -0800

    commit

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-console.log("Hello world!")
-
-var planOverviewEl = document.getElementById("plan-overview")
-
-function readTextFile(filename) {
+function embedText(filename, el) {
     fetch(filename)
         .then(results => results.text())
         .then(text => {
-            planOverviewEl.innerHTML = text
+            console.log(text)
+            el.innerHTML = text
         })
 }
 
-readTextFile("plan-overview.txt")
+function embedHTML(filename, el) {
+    fetch(filename)
+        .then(results => results.text())
+        .then(text => {
+            console.log(text)
+            el.outerHTML = text
+        })
+}
+
+var planOverviewEl = document.getElementById("plan-overview")
+embedText("plan-overview.txt", planOverviewEl)
+
+var structureChartEl = document.getElementById("stonks-structure-chart")
+embedHTML("Stonks-Structure-Chart.html", structureChartEl)
+
+var flowchartEl = document.getElementById("stonks-flowchart")
+embedHTML("Stonks-Flowchart.html", flowchartEl)
